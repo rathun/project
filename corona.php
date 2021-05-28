c9659bf12bd5935d34d3ef94c9b731a8309b3946
rathun <65532258+rathun@users.noreply.github.com>
2021-05-28 16:48:18 +0530

    Update corona.php

--- corona.php
+++ corona.php
@@ -12,7 +12,7 @@
 	<!-- STATE WISE live UPDATE -->
 	<div class="text-center text-danger"><h4>COVID-19 STATE WISE UPDATE</h4></div>
 	<br/>
-	<div class="table-responsive">
+	<div class="table table-responsive">
 	<table class="table table-bordered text-center bg-light">
 	
 		<!-- TABLE HEADING -->
